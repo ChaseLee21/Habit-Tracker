20da2337d7d8481cb62216c76945e1e3e0309e34
Tiny Penguin <chase.seeberger@gmail.com>
2024-02-07 16:38:29 -0800

    update to views and seeded data

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,27 +2,12 @@ const router = require('express').Router();
 const Stat = require('../models/Stat');
 
 // route to get all dishes
-router.get('/', async (req, res) => {
+router.get('/overview', async (req, res) => {
     const statData = await Stat.findAll().catch((err) => { 
       res.json(err);
     });
     const stats = statData.map((stat) => stat.get({ plain: true }));
-    res.render('all', { stats });
+    res.render('overview', { stats });
   });
-
-// route to get one dish
-router.get('/stat/:id', async (req, res) => {
-  try{ 
-      const statData = await Stat.findByPk(req.params.id);
-      if(!statData) {
-          res.status(404).json({message: 'No stat found with this id!'});
-          return;
-      }
-      const stat = statData.get({ plain: true });
-      res.render('stat', stat);
-    } catch (err) {
-        res.status(500).json(err);
-    };     
-});
 
 module.exports = router;
