1cf0840af74ac3464b9947c1f32223139d73ca10
Tiny Penguin <chase.seeberger@gmail.com>
2024-02-07 16:06:45 -0800

    updaing api route for stats

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
-// const dishRoutes = require('./dish-routes.js');
+const statRoutes = require('./stat-routes.js');
 
-// router.use('/dish', dishRoutes);
+router.use('/stat', statRoutes);
 
 module.exports = router;
