cd3329d6e5fa3f4258c6f024f1d0d1102c83c55b
Tiny Penguin <chase.seeberger@gmail.com>
2024-04-03 15:15:00 -0700

    added password hashing to user model

--- server/models/User.js
+++ server/models/User.js
@@ -1,13 +1,32 @@
 const { Schema, model } = require('mongoose');
+const crypto = require('crypto');
 
 const userSchema = new Schema({
     id: { type: String, required: true },
     name: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
-    habits: { type: Array, required: true }
+    habits: { type: Array, required: true },
+    salt: { type: String, required: true }
 });
 
-// TODO: Add a method to the schema to hash the password before saving it to the database
+userSchema.pre('save', function(next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+
+    // Generate a salt
+    this.salt = crypto.randomBytes(16).toString('hex');
+
+    // Hash the password
+    crypto.pbkdf2(this.password, this.salt, 1000, 64, 'sha512', (err, hash) => {
+        if (err) {
+            return next(err);
+        }
+
+        this.password = hash.toString('hex');
+        next();
+    });
+});
 
 module.exports = model('User', userSchema);
