c5bb5a31a7d8bffa881da1baea7832a60132132e
Tiny Penguin <chase.seeberger@gmail.com>
2024-04-03 15:05:51 -0700

    Created analytics model

---
+++ server/models/Analytics.js
@@ -0,0 +1,8 @@
+const { Schema, model } = require('mongoose');
+
+const analyticsSchema = new Schema({
+    date: { type: Date, required: true },
+    completed: { type: Boolean, required: true },
+});
+
+module.exports = model('Analytics', analyticsSchema);
