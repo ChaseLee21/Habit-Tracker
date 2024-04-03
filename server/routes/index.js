96c05dc4d62a46d9873d0914343a20539a9d94b1
Tiny Penguin <chase.seeberger@gmail.com>
2024-04-03 15:51:04 -0700

    cleaning up

---
+++ server/routes/index.js
@@ -0,0 +1,5 @@
+// Notes for next time:
+
+// just created a script to seed the data
+// database is currently seeded with three users, three habits, and six analytics
+// I now need to create api routes to interact with the database on the front end
