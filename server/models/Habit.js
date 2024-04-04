5c3992aa9657111209894231a2333b0bcb1e9095
Tiny Penguin <chase.seeberger@gmail.com>
2024-04-04 13:11:32 -0700

    updated habit schema to no longer include an id since mongodb uses a built in _id

--- server/models/Habit.js
+++ server/models/Habit.js
@@ -1,8 +1,6 @@
 const { Schema, model } = require('mongoose');
-const shortid = require('shortid');
 
 const habitSchema = new Schema({
-    id: { type: String, required: false },
     name: { type: String, required: true },
     description: { type: String, required: true },
     why: { type: String, required: true },
@@ -12,11 +10,6 @@ const habitSchema = new Schema({
     streak: { type: Number, required: false },
     longestStreak: { type: Number, required: false },
     analytics: { type: Array, required: false }
-});
-
-habitSchema.pre('save', function(next) {
-    this.id = shortid.generate();
-    next();
 });
 
 module.exports = model('Habit', habitSchema);
