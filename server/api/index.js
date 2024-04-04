eb6e8d1aa7a71870d99edf622be70e8c46601a29
Tiny Penguin <chase.seeberger@gmail.com>
2024-04-04 12:58:45 -0700

    initial habit-controller

--- server/api/index.js
+++ server/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userController = require('./user-controller');
+const habitController = require('./habit-controller');
 
 router.use('/users', userController);
+router.use('/habits', habitController);
 
 module.exports = router;
