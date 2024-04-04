b25c3ab03303bed135b1c88b96c69f6d79b74af9
Tiny Penguin <chase.seeberger@gmail.com>
2024-04-04 11:32:49 -0700

    post, put, and delete routes for user api controller

--- server/api/userRoutes.js
+++ server/api/userRoutes.js
@@ -15,7 +15,7 @@ router.get('/', (req, res) => {
 // GET one user by id
 router.get('/:id', (req, res) => {
     const { id } = req.params;
-    User.findOne({ id: id })
+    User.findOne({ _id: id })
         .then((user) => {
             res.json(user);
         })
@@ -25,9 +25,40 @@ router.get('/:id', (req, res) => {
 });
 
 // POST a new user
+router.post('/', (req, res) => {
+    const userData = req.body;
+    User.create(userData)
+        .then((user) => {
+            res.status(201).json({ message: 'User created successfully', user: user });
+        })
+        .catch((err) => {
+            res.status(400).json(err);
+        });
+})
 
 // PUT updated user by id
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const userData = req.body;
+    User.findOneAndUpdate(({ _id: id }, userData))
+        .then((user) => {
+            res.status(200).json({ message: 'User updated successfully', user: user});
+        })
+        .catch((err) => {
+            res.status(400).json(err);
+        });
+});
 
 // DELETE user by id
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    User.findOneAndDelete({ _id: id })
+        .then(() => {
+            res.status(200).json({ message: 'User deleted successfully' });
+        })
+        .catch((err) => {
+            res.status(400).json(err);
+        });
+});
 
 module.exports = router;
