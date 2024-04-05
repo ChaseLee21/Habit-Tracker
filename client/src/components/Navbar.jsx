89b30e9e6248d186bd37f38a7577a82d059eb831
Tiny Penguin <chase.seeberger@gmail.com>
2024-04-05 11:41:58 -0700

    adding navbar component to be used in header

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+function Navbar() {
+
+    return (
+      <>
+        <nav className="bg-secondary">
+            <ul className="m-2 p-2">
+                <li className="border border-black ps-2">Home</li>
+                <li className="border border-black ps-2">New Habit</li>
+                <li className="border border-black ps-2">Logout</li>
+            </ul>
+        </nav>
+      </>
+    )
+  }
+  
+  export default Navbar
+  
