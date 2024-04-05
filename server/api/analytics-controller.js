9a62035577609099e6490073a63e5e5340ba01fb
Tiny Penguin <chase.seeberger@gmail.com>
2024-04-04 17:01:58 -0700

    creating analytics-controller RESTful api

---
+++ server/api/analytics-controller.js
@@ -0,0 +1,65 @@
+const {Analytics} = require('../models/index');
+const router = require("express").Router();
+
+// GET all analytics
+router.get('/', (req, res) => {
+    Analytics.find({})
+        .then((analytics) => {
+            if (!analytics || analytics.length === 0) {
+                res.status(404).json({message: "No analytics were found"})
+                return;
+            }
+            res.json(analytics);
+        })
+        .catch((err) => {
+            res.status(400).json({message: err})
+        })
+});
+
+//GET one analytic by id
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    Analytics.findById({ _id: id})
+        .then((analytic) => {
+            if (!analytic) {
+                res.status(404).json({message: "No analytic was found with that id"})
+                return;
+            }
+            res.json(analytic);
+        })
+        .catch((err) => {
+            res.status(400).json({message: err})
+        })
+});
+
+//POST analytic
+router.post('/', (req, res) => {
+    Analytics.create(analyticData)
+    .then((analytic) => {
+        res.status(201).json({message: "analytic successfully created", analytic: analytic});
+    })
+    .catch((err) => {
+        res.status(400).json({message: err});
+    })
+});
+
+//PUT analytic
+router.put('/:id', (req, res) => {
+    const id = req.params.id
+    const analyticData = req.body
+    Analytics.findByIdAndUpdate({ _id: id }, { $set: analyticData })
+        .then((analytic) => {
+            if (!analytic) {
+                res.status(404).json({message: "No analytic was found with that id"})
+                return;
+            }
+            res.json({message: "analytic successfully updated", analytic: analytic});
+        })
+        .catch((err) => {
+            res.status(400).json({message: err});
+        })
+});
+
+//
+
+module.exports = router;
--- server/api/index.js
+++ server/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userController = require('./user-controller');
 const habitController = require('./habit-controller');
+const analyticsController = require('./analytics-controller');
 
 router.use('/users', userController);
 router.use('/habits', habitController);
+router.use('/analytics', analyticsController)
 
 module.exports = router;
