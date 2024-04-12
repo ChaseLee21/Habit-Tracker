5b2eeb5e1922cf403704754f9dedebd5dcd414cb
Tiny Penguin <chase.seeberger@gmail.com>
2024-04-12 13:16:43 -0700

    update: add habits property to User model so I can populate habits when the user is retrieved

--- server/models/User.js
+++ server/models/User.js
@@ -5,7 +5,8 @@ const userSchema = new Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true, select: false},
-    salt: { type: String, required: false, select: false}
+    salt: { type: String, required: false, select: false},
+    habits: [{ type: Schema.Types.ObjectId, ref: 'Habit' }]
 });
 
 // Middleware to hash the password before saving
