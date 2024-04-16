2fb2bc0fd1907135dc9b1ebf9271509b5917e6f7
Tiny Penguin <chase.seeberger@gmail.com>
2024-04-16 15:36:23 -0700

    styling footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,13 @@
 function Footer() {
 
     return (
-      <>
-        <h3>Footer</h3>
-      </>
+      <footer className="flex flex-col items-center rounded-md m-2 bg-secondaryBg text-secondaryText p-2 text-xl shadow-xl">
+        <h3>Made by Chase Seeberger</h3>
+        <div className="flex">
+          <p className="px-2">Github</p>
+          <p className="px-2">LinkedIn</p>
+        </div>
+      </footer>
     )
   }
   
