82b9db8dfcd66eb00d5500f80c342a3a14a18581
Tiny Penguin <chase.seeberger@gmail.com>
2024-04-17 12:08:30 -0700

    update: adding login, logout, and register links to navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,7 +5,9 @@ function Navbar() {
             <ul className="py-2 text-lg">
                 <li className=" p-1 ps-3 hover:text-highlight text-secondaryText">Home</li>
                 <li className="border-y p-1 ps-3 hover:text-highlight text-secondaryText">New Habit</li>
-                <li className=" p-1 ps-3 hover:text-highlight text-secondaryText">Logout</li>
+                <li className="p-1 ps-3 hover:text-highlight text-secondaryText">Login</li>
+                <li className="border-y p-1 ps-3 hover:text-highlight text-secondaryText">Logout</li>
+                <li className="p-1 ps-3 hover:text-highlight text-secondaryText">Register</li>
             </ul>
         </nav>
     )
