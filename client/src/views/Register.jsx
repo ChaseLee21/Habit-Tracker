edb3b0385a383aeb07b8639b1ce6eb8ef480a5f3
Tiny Penguin <chase.seeberger@gmail.com>
2024-04-17 15:42:02 -0700

    bug: fixed register form preventDefault not reloading the page

--- client/src/views/Register.jsx
+++ client/src/views/Register.jsx
@@ -5,26 +5,31 @@ function Register() {
 
   const handleRegisterSubmit = (e) => {
     e.preventDefault();
+    console.log(e);
     const email = e.target.email.value;
     const username = e.target.username.value;
     const password = e.target.password.value;
     const confirmPassword = e.target.confirmPassword.value;
     if (validateForm(email, username, password, confirmPassword)) {
-      createUser(email, username, password);
+      const userData = { email, username, password };
+      createUser(userData);
     }
   }
 
   function validateForm(email, username, password, confirmPassword) {
     if (!validateEmail(email)) {
       alert("Invalid email");
+      console.log("Invalid email");
       return false;
     }
     if (!validateUsername(username)) {
       alert("Invalid username");
+      console.log("Invalid username");
       return false;
     }
     if (!validatePassword(password, confirmPassword)) {
       alert("Invalid password");
+      console.log("Invalid password");
       return false;
     }
     return true;
@@ -33,7 +38,7 @@ function Register() {
   return (
     <>
       <h2 className="flex rounded-md m-2 bg-secondaryBg text-secondaryText p-2 text-xl shadow-xl">Register</h2>
-      <form className="flex flex-col m-2 p-2 bg-secondaryBg rounded-md shadow-xl" onSubmit={() => handleRegisterSubmit()}>
+      <form className="flex flex-col m-2 p-2 bg-secondaryBg rounded-md shadow-xl" onSubmit={handleRegisterSubmit}>
         <label htmlFor="email" className="text-secondaryText">Email:</label>
         <input type="email" id="email" name="email" className="p-1 m-1 rounded-md" />
         <label htmlFor="username" className="text-secondaryText">Username:</label>
@@ -41,7 +46,7 @@ function Register() {
         <label htmlFor="password" className="text-secondaryText">Password:</label>
         <input type="password" id="password" name="password" className="p-1 m-1 rounded-md" />
         <label htmlFor="confirm-password" className="text-secondaryText">Confirm Password:</label>
-        <input type="password" id="confirm-password" name="confirm-password" className="p-1 m-1 rounded-md" />
+        <input type="password" id="confirmPassword" name="confirmPassword" className="p-1 m-1 rounded-md" />
         <button type="submit" className="bg-primaryBg text-primaryText w-fit p-2 rounded-md m-1">Register</button>
       </form>
     </>
