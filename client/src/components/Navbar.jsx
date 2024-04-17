ce27e42063fc1e09416c53f1c1583ca8921d8f87
Tiny Penguin <chase.seeberger@gmail.com>
2024-04-17 12:36:15 -0700

    feature: implementing Links into navbar from react router dom

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,13 +1,25 @@
+import { Link } from "react-router-dom"
+
 function Navbar() {
 
     return (
         <nav className="bg-secondaryBg">
             <ul className="py-2 text-lg">
-                <li className=" p-1 ps-3 hover:text-highlight text-secondaryText">Home</li>
-                <li className="border-y p-1 ps-3 hover:text-highlight text-secondaryText">New Habit</li>
-                <li className="p-1 ps-3 hover:text-highlight text-secondaryText">Login</li>
-                <li className="border-y p-1 ps-3 hover:text-highlight text-secondaryText">Logout</li>
-                <li className="p-1 ps-3 hover:text-highlight text-secondaryText">Register</li>
+                <li className="p-1 ps-3 hover:text-highlight text-secondaryText">
+                    <Link to={'/'}>Home</Link>
+                </li>
+                <li className="border-y p-1 ps-3 hover:text-highlight text-secondaryText">
+                    <Link to={'/new-habit'}>New Habit</Link>
+                </li>
+                <li className="p-1 ps-3 hover:text-highlight text-secondaryText">
+                    <Link to={'/login'}>Login</Link>
+                </li>
+                <li className="border-y p-1 ps-3 hover:text-highlight text-secondaryText">
+                    <Link to={'/register'}>Register</Link>
+                </li>
+                <li className="p-1 ps-3 hover:text-highlight text-secondaryText">
+                    Logout
+                </li>
             </ul>
         </nav>
     )
