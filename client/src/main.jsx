ef05d3b07629df8caa692e0fc6f541453a494ecf
Tiny Penguin <chase.seeberger@gmail.com>
2024-04-17 12:15:59 -0700

    update: implemented react router dom in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import './axiosConfig.js'
+import Home from './views/Home.jsx'
+import Register from './views/Register.jsx'
+import Login from './views/Login.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <h1 className='display-2'>Wrong page!</h1>,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      }, 
+      {
+        path: '/register',
+        element: <Register />
+      },
+      {
+        path: '/login',
+        element: <Login />
+      },
+    ]
+  }
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
