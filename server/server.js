354863f7f31a6b442d91371f21fce5ed4a9f4cc9
Tiny Penguin <chase.seeberger@gmail.com>
2024-06-17 13:28:35 -0700

    applying eslint rules to express server file

--- server/server.js
+++ server/server.js
@@ -1,34 +1,32 @@
-const express = require('express');
-const db = require('./config/connection.js');
-const api = require('./api/index.js');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
+const express = require('express')
+const db = require('./config/connection.js')
+const api = require('./api/index.js')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
 
 const PORT = 3000
 
 // Create an Express app
-const app = express();
+const app = express()
 
 // Middleware to allow react to connect to the server during development
 app.use(cors({
     origin: 'http://localhost:5173',
     credentials: true
-}));
+}))
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.use(cookieParser());
+app.use(cookieParser())
 
-//TODO: server static files
-
-app.use('/api', api);
+app.use('/api', api)
 
 // Connect to MongoDB
 db.once('open', () => {
-    console.log('Connected to MongoDB');
+    console.log('Connected to MongoDB')
 
     app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
-});
+        console.log(`Server is running on port ${PORT}`)
+    })
+})
