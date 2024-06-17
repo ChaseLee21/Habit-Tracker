856867b77a3c6f0367a4b03fb8f5f6644608c503
Tiny Penguin <chase.seeberger@gmail.com>
2024-06-17 14:08:11 -0700

    added logout component to router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,34 +6,39 @@ import './index.css'
 import './axiosConfig.js'
 import Register from './views/Register.jsx'
 import Login from './views/Login.jsx'
+import Logout from './views/Logout.jsx'
 import { ProtectedHome, ProtectedNewHabit } from './util/auth/auth.js'
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    errorElement: <h1 className='display-2'>Wrong page!</h1>,
-    children: [
-      {
-        index: true,
-        element: <ProtectedHome />
-      },
-      {
-        path: '/register',
-        element: <Register />
-      },
-      {
-        path: '/login',
-        element: <Login />
-      },
-      {
-        path: '/new-habit',
-        element: <ProtectedNewHabit />
-      }
-    ]
-  }
+    {
+        path: '/',
+        element: <App />,
+        errorElement: <h1 className='display-2'>Wrong page!</h1>,
+        children: [
+            {
+                index: true,
+                element: <ProtectedHome />
+            },
+            {
+                path: '/register',
+                element: <Register />
+            },
+            {
+                path: '/login',
+                element: <Login />
+            },
+            {
+                path: '/logout',
+                element: <Logout />
+            },
+            {
+                path: '/new-habit',
+                element: <ProtectedNewHabit />
+            }
+        ]
+    }
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
+    <RouterProvider router={router} />
 )
