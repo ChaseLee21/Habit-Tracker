21d68c2a4b55370e3df6651d345dd33c5156407b
Tiny Penguin <chase.seeberger@gmail.com>
2024-06-17 12:36:51 -0700

    applying eslint rules

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
-const { connect, connection } = require('mongoose');
+const { connect, connection } = require('mongoose')
 
-const connectionString = 'mongodb://127.0.0.1:27017/life-stats-tracker-db';
+const connectionString = 'mongodb://127.0.0.1:27017/life-stats-tracker-db'
 
-connect(connectionString);
+connect(connectionString)
 
-module.exports = connection;
+module.exports = connection
