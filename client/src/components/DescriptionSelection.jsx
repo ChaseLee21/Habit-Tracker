3e8494a3856d021aef8fd5142e7a45688e0fefa5
Tiny Penguin <chase.seeberger@gmail.com>
2024-06-19 11:36:46 -0700

    update: description component

---
+++ client/src/components/DescriptionSelection.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function DescriptionSelection ({ descriptions, onItemClick }) {
+    return (
+        <ul>
+            {descriptions.map((description, index) => {
+                return (
+                    <li key={index} className='m-2 p-2 bg-primaryBg text-primaryText rounded-md shadow-md' onClick={() => onItemClick(description)}>
+                        {description}
+                    </li>
+                )
+            })}
+        </ul>
+    )
+}
+
+DescriptionSelection.propTypes = {
+    descriptions: PropTypes.arrayOf(String).isRequired,
+    onItemClick: PropTypes.func.isRequired
+}
+
+export default DescriptionSelection
