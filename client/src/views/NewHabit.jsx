689bb2e9c94dd0bfa02cb257170ecd8d20e1c20c
Tiny Penguin <chase.seeberger@gmail.com>
2024-06-19 11:21:00 -0700

    update: added an event handler for when the user selects a habit

--- client/src/views/NewHabit.jsx
+++ client/src/views/NewHabit.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { React, useState, useEffect } from 'react'
 import StartingPointList from '../components/StartingPointList'
 
 function NewHabit () {
+    let habit;
+
+    useEffect(() => {
+        const localStorageHabit = JSON.parse(localStorage.getItem('newHabit'))
+        if (localStorageHabit) {
+            const contiueWithHabit = window.confirm(`You left off creating a new habit in progress. Do you want to continue with ${localStorageHabit.name}?`)
+            if (contiueWithHabit) {
+                habit = localStorageHabit
+                // TODO: when user confirms to continue, set the page to show the next step they were on
+            }
+        }
+    }, [])
+
+    // Starting Points
+    const [showStartingPointList, setShowStartingPointList] = useState(true)
     const startingPoints = [
         {
             name: 'Exercise',
@@ -51,15 +66,19 @@ function NewHabit () {
         }
     ]
 
-    const handleItemClick = (habit) => {
-        console.log(habit)
+
+
+    const handleStartingPointSelection = (habit) => {
+        localStorage.setItem('newHabit', JSON.stringify(habit))
+        setShowStartingPointList(false)
     }
 
     return (
         <section className="flex flex-col rounded-md m-2 bg-secondaryBg text-secondaryText p-2 shadow-xl">
-            <h2 className='text-xl'>New Habit</h2>
-            <p>To help accelerate the process of creating a new habit we gave you some starting points.</p>
-            <StartingPointList startingPoints={startingPoints} onItemClick={handleItemClick} />
+            <h2 className='text-xl'>Create a New Habit</h2>
+            <p>To help accelerate the process of creating a new habit we gave you some starting points.
+            You will make goals and define your habit soon, this is just a starting point to get you going!</p>
+            {showStartingPointList && <StartingPointList startingPoints={startingPoints} onItemClick={handleStartingPointSelection} />}
         </section>
     )
 }
