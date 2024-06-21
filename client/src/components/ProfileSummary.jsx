4f256d8638feb00c51e4eb00c2a4752d25a744e4
Tiny Penguin <chase.seeberger@gmail.com>
2024-06-21 10:04:04 -0700

    adding an edit button to habits

--- client/src/components/ProfileSummary.jsx
+++ client/src/components/ProfileSummary.jsx
@@ -4,16 +4,22 @@ import PropTypes from 'prop-types'
 function ProfileSummary (props) {
     const user = props.user
 
+    function handleEditHabit (habit) {
+        console.log('Edit habit: ', habit)
+    }
+
     return (
         <section>
             <p>Username: {user.name}</p>
             <p>Email: {user.email}</p>
             <p>Timezone: {user.timezone}</p>
-            <p>Habits:</p>
             <ul>
                 {user.habits && user.habits.map(habit => {
-                    return <li className='bg-primaryBg text-primaryText rounded p-2 w-fit' key={habit._id}>
-                        <p>{habit.name}</p>
+                    return <li className='bg-primaryBg text-primaryText rounded p-2 my-2 w-fit' key={habit._id}>
+                        <div className='flex justify-between'>
+                            <p>{habit.name}</p>
+                            <button className='bg-primaryBg text-primaryText rounded px-2 w-fit hover:underline' onClick={() => handleEditHabit(habit)}>Edit</button>
+                        </div>
                         <p>{habit.description}</p>
                         <p>{habit.why}</p>
                         <p>{habit.goal}</p>
