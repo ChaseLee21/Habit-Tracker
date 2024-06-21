167b5a5e718b3ea511f65e270b4f87cf1b0c994f
Tiny Penguin <chase.seeberger@gmail.com>
2024-06-21 10:15:05 -0700

    update: edit habit form

--- client/src/components/HabitEdit.jsx
+++ client/src/components/HabitEdit.jsx
@@ -4,16 +4,20 @@ import PropTypes from 'prop-types'
 function HabitEdit (habit) {
     return (
         <section>
-            <ul>
-                <li className='bg-primaryBg text-primaryText rounded p-2 my-2 w-fit'>
-                    <p>{habit.name}</p>
-                    <p>{habit.description}</p>
-                    <p>{habit.why}</p>
-                    <p>{habit.goal}</p>
-                    <p>{habit.frequency} times a week</p>
-                    {habit.reward && <p>Reward: {habit.reward}</p>}
-                </li>
-            </ul>
+            <form>
+                <label htmlFor='habitName'>Habit Name:</label>
+                <input id='habitName' type='text' defaultValue={habit.name}></input>
+                <label htmlFor='habitDescription'>Habit Description:</label>
+                <input id='habitDescription' type='text' defaultValue={habit.description}></input>
+                <label htmlFor='habitWhy'>Habit Why:</label>
+                <input id='habitWhy' type='text' defaultValue={habit.why}></input>
+                <label htmlFor='habitGoal'>Habit Goal:</label>
+                <input id='habitGoal' type='text' defaultValue={habit.goal}></input>
+                <label htmlFor='habitFrequency'>Habit Frequency:</label>
+                <input id='habitFrequency' type='text' defaultValue={habit.frequency}></input>
+                <label htmlFor='habitReward'>Habit Rewards:</label>
+                <input id='habitReward' type='text' defaultValue={habit.reward}></input>
+            </form>
         </section>
     )
 }
