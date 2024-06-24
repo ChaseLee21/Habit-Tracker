748dec05c1ce8f71fae088369ec5362e3ef59994
Tiny Penguin <chase.seeberger@gmail.com>
2024-06-24 11:28:07 -0700

    update: removed properties from day schema that were not needed

--- server/models/Day.js
+++ server/models/Day.js
@@ -2,7 +2,6 @@ const { Schema, model } = require('mongoose')
 
 const daySchema = new Schema({
     week: { type: Schema.Types.ObjectId, ref: 'Week', required: true },
-    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
     date: { type: String, required: true },
     completed: { type: Boolean, required: true }
 })
