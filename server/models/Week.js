3baf6ce1d43e379860aadae31cd36f7f034b3246
Tiny Penguin <chase.seeberger@gmail.com>
2024-06-24 13:55:45 -0700

    refactor: remove required property for frequency and endDate in Week model

--- server/models/Week.js
+++ server/models/Week.js
@@ -4,9 +4,9 @@ const { setEndDate, setDaysArray } = require('../utils/helpers')
 const weekSchema = new Schema({
     habit: { type: Schema.Types.ObjectId, ref: 'Habit', required: true },
     user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    frequency: { type: Number, required: true },
+    frequency: { type: Number },
     days: [{ type: Schema.Types.ObjectId, ref: 'Day' }],
-    endDate: { type: Date, required: true }
+    endDate: { type: Date }
 })
 
 // 0 is Sunday, 6 is Saturday
