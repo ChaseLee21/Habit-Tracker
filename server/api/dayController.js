2a69dbd56af36be5a7078f1d0ca2f90304d27a19
Tiny Penguin <chase.seeberger@gmail.com>
2024-06-24 11:43:25 -0700

    update: refactored analytics controller to be the day controller

---
+++ server/api/dayController.js
@@ -0,0 +1,25 @@
+const { Day } = require('../models/index')
+const router = require('express').Router()
+
+//  PUT analytic by id (used to update the completed status)
+router.put('/:id', async (req, res) => {
+    const id = req.params.id
+    const dayData = req.body
+    try {
+        const day = await Day.findOneAndUpdate({ _id: id }, dayData, { new: true })
+        if (!day) {
+            res.status(404).json({ message: 'No day was found with that id' })
+            return
+        }
+        if (day.completed === true) {
+            // Using the week the day is in, find the habit and increment the streak
+        }
+        await day.save()
+        res.status(200).json(day)
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({ message: 'An error has occurred while attempting to update the day' })
+    }
+})
+
+module.exports = router
