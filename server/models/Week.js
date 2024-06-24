1806da58e4cf9d9448052882dafc9ad39f0ca276
Tiny Penguin <chase.seeberger@gmail.com>
2024-06-24 11:29:29 -0700

    update: new endDate property in Week schema

--- server/models/Week.js
+++ server/models/Week.js
@@ -4,7 +4,8 @@ const weekSchema = new Schema({
     habit: { type: Schema.Types.ObjectId, ref: 'Habit', required: true },
     user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
     frequency: { type: Number, required: true },
-    days: [{ type: Schema.Types.ObjectId, ref: 'Day' }]
+    days: [{ type: Schema.Types.ObjectId, ref: 'Day' }],
+    endDate: { type: Date, required: true }
 })
 
 module.exports = model('Habit', weekSchema)
