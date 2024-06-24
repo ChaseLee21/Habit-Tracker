084141c8b503eda4e1468509550fe337270d588b
Tiny Penguin <chase.seeberger@gmail.com>
2024-06-24 12:26:00 -0700

    update: habit schema now creates an instance of the week schema on initialization

--- server/models/Habit.js
+++ server/models/Habit.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose')
+const { Week } = require('./index')
 
 const habitSchema = new Schema({
     user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
@@ -10,6 +11,12 @@ const habitSchema = new Schema({
     frequency: { type: Number, required: true },
     streak: { type: Number, required: false },
     weeks: [{ type: Schema.Types.ObjectId, ref: 'Week' }]
+})
+
+habitSchema.on('init', async function () {
+    this.streak = 0
+    const firstWeek = await Week.create({ habit: this._id, user: this.user })
+    this.weeks.push(firstWeek)
 })
 
 module.exports = model('Habit', habitSchema)
