e9a8384c011dca75ea8dfaaabfe0e220e49b8437
Tiny Penguin <chase.seeberger@gmail.com>
2024-06-25 12:10:12 -0700

    refactor: Update ErrorAlert component to include a close button

--- client/src/components/ErrorAlert.jsx
+++ client/src/components/ErrorAlert.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import propTypes from 'prop-types'
 
-function ErrorAlert ({ message }) {
+function ErrorAlert ({ message, handleCloseAlert }) {
     return (
-        <div className="bg-red-500 text-white p-2 rounded-md shadow-xl">
+        <div className="bg-red-500 text-white p-2 m-2 rounded-md shadow-xl flex justify-between">
             {message}
+            <button type='button' className="text-white" onClick={() => handleCloseAlert()}>X</button>
         </div>
     )
 }
 
 ErrorAlert.propTypes = {
-    message: propTypes.string.isRequired
+    message: propTypes.string.isRequired,
+    handleCloseAlert: propTypes.func.isRequired
 }
 
 export default ErrorAlert
