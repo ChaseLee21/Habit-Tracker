a91aa67585c75145cf772dc0a1b54f55c5480bea
Tiny Penguin <chase.seeberger@gmail.com>
2024-06-25 13:09:01 -0700

    refactor: Update goal input ID in GoalSelection component

--- client/src/components/new-habit/GoalSelection.jsx
+++ client/src/components/new-habit/GoalSelection.jsx
@@ -10,7 +10,7 @@ function GoalSelection ({ onItemClick }) {
         <div className='flex flex-col text-lg'>
             <div className='flex flex-wrap'>
                 <span className='w-fit mx-1'>I finished my habit for the day when I </span>
-                <input className='rounded px-1 w-auto text-black' id='whyInput' type='text' max='100' onInput={handleInputChange}></input>
+                <input className='rounded px-1 w-auto text-black' id='goalInput' type='text' max='100' onInput={handleInputChange}></input>
             </div>
             <button className='bg-primaryBg text-primaryText rounded w-fit p-1 my-2' onClick={() => onItemClick(goal)}>Next</button>
         </div>
