753ee94e22489e97a11baedbee55cae1ff85298a
Tiny Penguin <chase.seeberger@gmail.com>
2024-06-27 11:31:15 -0700

    update: adding shadow to desktop navbar

--- client/src/components/nav/NavBarDesktop.jsx
+++ client/src/components/nav/NavBarDesktop.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 function Header () {
     return (
-        <header className="flex flex-col justify-between bg-colorPrimary text-colorSecondary min-h-[100vh]">
+        <header className="flex flex-col justify-between bg-colorPrimary text-colorSecondary min-h-[100vh] shadow-md shadow-colorShadow">
             <div className="p-2">
                 <h1 className="text-colorSecondary hover:text-colorSecondaryDark text-2xl text-center">
                     <Link to={'/'}>Habit Tracker</Link>
