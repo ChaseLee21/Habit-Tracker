6e3e9b83243755ab051ad055eb799dc00c49fd57
Tiny Penguin <chase.seeberger@gmail.com>
2024-06-27 09:53:14 -0700

    refactor: Update Footer component to fix positioning and styling

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Footer () {
     return (
-        <footer className="flex flex-col items-center justify-end bg-colorBg text-colorText">
-            <h3>Made by Chase Seeberger</h3>
+        <footer className="flex flex-col items-center justify-end text-colorText fixed bottom-0 bg-transparent w-full text-center">
+            <p>Made by Chase Seeberger</p>
             <div className="flex">
                 <a className="px-2 text-colorLink hover:text-colorLinkHover" target='_blank' rel="noreferrer" href='https://github.com/ChaseLee21'>Github</a>
                 <a className="px-2 text-colorLink hover:text-colorLinkHover" target='_blank' rel="noreferrer" href='https://www.linkedin.com/in/chaseseeberger/'>LinkedIn</a>
