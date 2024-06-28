489b09294f7e18ad5325ae6e28accf0418dfd141
Tiny Penguin <chase.seeberger@gmail.com>
2024-06-28 15:46:51 -0700

    updating axios config to no longer only use localhost

--- client/src/axiosConfig.js
+++ client/src/axiosConfig.js
@@ -1,2 +1,8 @@
 import axios from 'axios'
-axios.defaults.baseURL = 'http://localhost:3000'
+
+// Determine the base URL based on the environment
+const baseURL = process.env.NODE_ENV === 'production'
+    ? 'https://habit-tracker-jukt.onrender.com'
+    : 'http://localhost:3000'
+
+axios.defaults.baseURL = baseURL
