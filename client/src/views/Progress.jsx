93c976e465c5a4599f477bf2abe8cf48b98c2759
Tiny Penguin <chase.seeberger@gmail.com>
2024-07-01 10:44:07 -0700

    refactor: Update Progress component to include PropTypes for user prop

--- client/src/views/Progress.jsx
+++ client/src/views/Progress.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react'
+import PropTypes from 'prop-types'
 
-function Progress () {
+function Progress (props) {
+    // const userId = props.user.user.id || ''
+
     useEffect(() => {
         document.title = 'Progress | Habit Tracker'
     }, [])
@@ -10,6 +13,14 @@ function Progress () {
             <h2>Your Habit Progress:</h2>
         </>
     )
+}
+
+Progress.propTypes = {
+    user: PropTypes.shape({
+        user: PropTypes.shape({
+            id: PropTypes.string
+        })
+    }).isRequired
 }
 
 export default Progress
