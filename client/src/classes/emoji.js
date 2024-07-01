e5405cd1968f8bb6c378afbc6816b4eedda5129c
Tiny Penguin <chase.seeberger@gmail.com>
2024-07-01 16:42:19 -0700

    refactor: Update Emoji class to include gravity, friction, and collision detection

--- client/src/classes/emoji.js
+++ client/src/classes/emoji.js
@@ -6,25 +6,42 @@ class Emoji {
         this.velX = velX
         this.velY = velY
         this.size = size
-        this.gravity = 0.1
+        this.gravity = 0.9
+        this.friction = 0.9
     }
 
-    draw (ctx) {
+    draw(ctx) {
+        // Draw the emoji
         ctx.font = `${this.size}px "Segoe UI Emoji", sans-serif`
         ctx.fillText(this.emoji, this.x, this.y)
+        // Set border color and thickness
+        ctx.strokeStyle = 'black'
+        ctx.lineWidth = 1 
+        const rectX = this.x
+        const rectY = this.y 
+        const rectWidth = this.size
+        const rectHeight = this.size
+        ctx.strokeRect(rectX, rectY, rectWidth, rectHeight)
     }
 
     update (canvas) {
         if (this.x + this.size > canvas.width || this.x < 0) {
             this.velX = -this.velX
+            if (this.velX === 0) {
+                this.velX += .03
+            }
         }
 
         if (this.y + this.size > canvas.height || this.y < 0) {
             this.velY = -this.velY
+            this.velY = this.velY * this.friction
         }
 
         if (this.y + this.size < canvas.height) {
             this.velY += this.gravity
+        } else if (this.y + this.size >= canvas.height){
+            // Friction applied to emojis X Velocity when emoji is on the ground
+            this.velX = this.velX * this.friction
         }
 
         this.x += this.velX
@@ -38,11 +55,20 @@ class Emoji {
                 const dy = this.y - emojis[j].y
                 const distance = Math.sqrt(dx * dx + dy * dy)
 
-                if (distance < this.size + emojis[j].size) {
-                    emojis[j].velX = -emojis[j].velX
-                    emojis[j].velY = -emojis[j].velY
+                if (distance <= this.size) {
+                    // Reverse velocities
                     this.velX = -this.velX
-                    this.velY = -this.velY
+                    emojis[j].velX = -emojis[j].velX
+                
+                    // Move emojis apart to reduce overlap
+                    const overlap = (this.size) - distance
+                    const adjustX = (overlap / 2) * (dx / distance)
+                    const adjustY = (overlap / 2) * (dy / distance)
+                
+                    this.x += adjustX
+                    this.y += adjustY
+                    emojis[j].x -= adjustX
+                    emojis[j].y -= adjustY
                 }
             }
         }
