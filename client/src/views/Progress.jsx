69337d866d615b1a5eb58971b1fa1e03d1b20ca9
Tiny Penguin <chase.seeberger@gmail.com>
2024-07-11 14:49:13 -0700

    refactor: implementing matter.js to the progress component

--- client/src/views/Progress.jsx
+++ client/src/views/Progress.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import { getUser } from '../util/axios'
+import { Engine, Render, Runner, Composites, MouseConstraint, Mouse, Composite, Bodies } from 'matter-js'
 
 function Progress (props) {
     const userId = props.user.user.id || ''
     const [user, setUser] = useState({})
-    // const testEmojis = '🍎🍏🍊🍋🍌🍉🍇🍓🍈🍒🍑🥭🍍🥥🧀🥚🍳🥞🥓🥩🍗🍖🌭🍔🍟🍕🥪🥙🌮🌯🥗🥘🥫🍝🍜🍲🍛🍣🍱🥟🍤🍙🍚🍘🍥🥠🍢🍡🍧🍨🍦🥧🧁🍰🎂🍮🍭🍬🍫🍿🍩🍪🌰🥜🍯🥛🍼🍵🍶🍺🍻🥂🍷🥃🍸🍹🍾🥤🧃🧉🧊🥢🍽🍴🥄🔪🏺🌍🌎🌏🌐🗺🗾🧭'
+    const testEmojis = '🍎🍏🍊🍋🍌🍉🍇🍓🍈🍒🍑🥭🍍🥥🧀🥚🍳🥞🥓🥩🍗🍖🌭🍔🍟🍕🥪🥙🌮🌯🥗🥘🥫🍝🍜🍲🍛🍣🍱🥟🍤🍙🍚🍘🍥🥠🍢🍡🍧🍨🍦🥧🧁🍰🎂🍮🍭🍬🍫🍿🍩🍪🌰🥜🍯🥛🍼🍵🍶🍺🍻🥂🍷🥃🍸🍹🍾🥤🧃🧉🧊🥢🍽🍴🥄🔪🏺🌍🌎🌏🌐🗺🗾🧭'
 
+    // Retrieve user data
     useEffect(() => {
         async function fetchUser () {
             try {
@@ -22,26 +24,71 @@ function Progress (props) {
         }
         fetchUser()
     }, [])
+
+    // Set page title
+    useEffect(() => {
+        document.title = 'Progress | Habit Tracker'
+    }, [])
     
+    // Initialize canvas after user data is retrieved
     useEffect(() => {
         if (user.emojis) {
             initCanvas()
         }
     }, [user.emojis])
 
-    useEffect(() => {
-        document.title = 'Progress | Habit Tracker'
-    }, [])
 
     function initCanvas () {
+        // create engine
+        const engine = Engine.create()
+        const world = engine.world
+        const render = Render.create({
+            element: document.querySelector('#canvas'),
+            engine: engine,
+            options: {
+                width: 800,
+                height: 600,
+                background: '#FFF',
+                wireframes: false
+            }
+        })
+        Render.run(render)
+
+        // create runner
+        var runner = Runner.create()
+        Runner.run(runner, engine)
+
+        let renderEmojis = []
+        for (let emoji of testEmojis) {
+            const spriteUrl = `https://emojicdn.elk.sh/${encodeURIComponent(emoji)}`
+            const x = Math.random() * 800
+            const y = 0
+            renderEmojis.push(Bodies.circle(x, y, 20, {render: {sprite: {texture: spriteUrl, xScale: 0.25, yScale: 0.25}}}))
+        }
+
+        // walls
+        Composite.add(world, [
+            Bodies.rectangle(400, 0, 800, 50, { isStatic: true }),
+            Bodies.rectangle(400, 600, 800, 50, { isStatic: true }),
+            Bodies.rectangle(800, 300, 50, 600, { isStatic: true }),
+            Bodies.rectangle(0, 300, 50, 600, { isStatic: true }),
+        ])
+
+        //emojis
+        Composite.add(world, renderEmojis)
+
+        // fit the render viewport to the scene
+        Render.lookAt(render, {
+            min: { x: 0, y: 0 },
+            max: { x: 800, y: 600 }
+        })
     }
 
     return (
-        <main id='canvasContainer' className='m-2 grid grid-cols-5'>
-            <canvas className='border border-black col-span-5' >
-
-            </canvas>
+        <main id='canvas' className='m-2 grid grid-cols-5'>
+        
         </main>
+
     )
 }
 
