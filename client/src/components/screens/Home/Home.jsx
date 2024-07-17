803b507714694b3f1e437b86bc7815069a26b82c
Tiny Penguin <chase.seeberger@gmail.com>
2024-07-17 16:16:29 -0700

    updating home component to fix bugs that occured when moving around lower level components

--- client/src/components/screens/Home/Home.jsx
+++ client/src/components/screens/Home/Home.jsx
@@ -15,34 +15,39 @@ function Home (props) {
         document.title = 'Home | Habit Tracker'
     }, [])
     
-    async function handleConfirmUpdate () {
-        setShowConfirmUpdate(false)
-        setShowUpdateGoal(true)
-    }
+    // async function handleConfirmUpdate () {
+    //     setShowConfirmUpdate(false)
+    //     setShowUpdateGoal(true)
+    // }
 
-    function handleCancelUpdate () {
-        updateHabitToUpdate(undefined)
-    }
+    // function handleCancelUpdate () {
+    //     updateHabitToUpdate(undefined)
+    // }
 
     async function handleUpdateGoalSubmit (habit) {
         await updateHabitGoalState(habit)
-        setShowUpdateGoal(false)
+        updateHabitToUpdate(undefined)
+    }
+
+    function handleUpdateGoalCancel () {
+        updateHabitToUpdate(undefined)
     }
 
     return (
         <main className="flex flex-col bg-colorBg text-colorText">
             <Day {...props}/>
             <Habits {...props} />
-            {showUpdateGoal && <section className='h-full w-full fixed top-0 left-0 bg-gray-800 bg-opacity-50 grid grid-flow-col grid-cols-6'>
+            {habitToUpdate && habitToUpdate !== undefined && <section className='h-full w-full fixed top-0 left-0 bg-gray-800 bg-opacity-50 grid grid-flow-col grid-cols-6'>
                 <div className='col-span-6 md:col-span-4 md:col-start-2'>
-                    <UpdateGoal habit={habitToUpdate} onCancel={() => setShowUpdateGoal(false)} onConfirm={handleUpdateGoalSubmit} />
+                    <UpdateGoal habit={habitToUpdate} onCancel={handleUpdateGoalCancel} onConfirm={handleUpdateGoalSubmit} />
                 </div>
             </section>}
-            {habitToUpdate && habitToUpdate !== undefined && <section className='h-full w-full fixed top-0 left-0 bg-gray-800 bg-opacity-50 grid grid-flow-col grid-cols-6'>
+            {/* removed until I refactor it as a useEffect hook inside the UpdateGoal Component */}
+            {/* <section className='h-full w-full fixed top-0 left-0 bg-gray-800 bg-opacity-50 grid grid-flow-col grid-cols-6'>
                 <div className='col-span-6 md:col-span-4 md:col-start-2'>
                     <ConfirmUpdate habit={habitToUpdate} onConfirm={handleConfirmUpdate} onCancel={handleCancelUpdate} />
                 </div>
-            </section>}
+            </section> */}
         </main>
     )
 }
