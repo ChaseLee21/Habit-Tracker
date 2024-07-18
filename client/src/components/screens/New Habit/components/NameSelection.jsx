14a79a3bbe2c1990f4268070286f31bff91d2d2d
Tiny Penguin <chase.seeberger@gmail.com>
2024-07-18 16:13:03 -0700

    feature: name selection component now has a back button

--- client/src/components/screens/New Habit/components/NameSelection.jsx
+++ client/src/components/screens/New Habit/components/NameSelection.jsx
@@ -3,7 +3,7 @@ import SectionHeader from './SectionHeader'
 import { useNewHabit } from '../../../../contexts/NewHabitContext'
 
 function NameSelection () {
-    const { updateHabit, updateDescriptions, updateShowNameSelection, updateShowDescriptionSelection } = useNewHabit()
+    const { updateHabit, updateDescriptions, updateShowNameSelection, updateShowDescriptionSelection, updateShowHabitSelection } = useNewHabit()
     const [name, setName] = useState('')
 
     const handleInputChange = (e) => {
@@ -18,12 +18,18 @@ function NameSelection () {
         updateShowNameSelection(false)
     }
 
+    function back () {
+        updateShowHabitSelection(true)
+        updateShowNameSelection(false)
+    }
+    
     return (
         <div className='p-2 bg-colorBgAlt rounded-md'>
             <SectionHeader title='Name Your Habit' subtext={`This should be short and simple such as: 'Exercise', 'Read', 'Drink Water', Etc.`} />
             <div className='flex flex-wrap my-2'>
                 <input className='rounded w-full px-1' id='whyInput' type='text' max='100' onInput={handleInputChange}></input>
             </div>
+            <button className='bg-colorButtonBgAlt text-colorButtonTextAlt hover:text-colorLinkHover rounded w-fit p-1 m-2' onClick={() => back()}>Back</button>
             {name && <button type='button' className='bg-colorButtonBgAlt text-colorButtonTextAlt rounded w-fit p-1 cursor-pointer my-2' onClick={() => next()}>Next</button>}
             {!name && <button className='bg-gray-600 text-gray-50 rounded w-fit p-1 my-2' disabled>Next</button>}
         </div>
