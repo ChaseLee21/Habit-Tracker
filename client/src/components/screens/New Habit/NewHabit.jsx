a8104a49288a2475b99d84e5f16a01a7fa08b684
Tiny Penguin <chase.seeberger@gmail.com>
2024-07-18 15:12:39 -0700

    cleaning: removed unused code from new habit component

--- client/src/components/screens/New Habit/NewHabit.jsx
+++ client/src/components/screens/New Habit/NewHabit.jsx
@@ -15,65 +15,12 @@ import HabitSummary from './components/HabitSummary'
 function NewHabit (props) {
     // Variables
     const userId = props.user.user.id || ''
-    const { habit, updateHabit, showHabitSelection, showNameSelection, showDescriptionSelection, showWhySelection, showGoalSelection, showFrequencySelection, showEmojiSelection, showHabitSummary } = useNewHabit()
+    const { showHabitSelection, showNameSelection, showDescriptionSelection, showWhySelection, showGoalSelection, showFrequencySelection, showEmojiSelection, showHabitSummary } = useNewHabit()
 
     // Lifecycle Methods
     useEffect(() => {
         document.title = 'New Habit | Habit Tracker'
     }, [])
-
-    const setHabitDescription = (description) => {
-        updateHabit({ ...habit, description })
-        setShowDescriptionSelection(false)
-        setShowWhySelection(true)
-        setSectionHeader({
-            title: 'Why',
-            subtext: `I will ${description} because... `
-        })
-    }
-    const setHabitWhy = (why) => {
-        updateHabit({ ...habit, why })
-        setShowWhySelection(false)
-        setShowGoalSelection(true)
-        setSectionHeader({
-            title: 'Create a Goal',
-            subtext: 'I finish my habit for the day when'
-        })
-    }
-    const setHabitGoal = (goal) => {
-        updateHabit({ ...habit, goal })
-        setShowGoalSelection(false)
-        setShowFrequencySelection(true)
-        setSectionHeader({
-            title: 'How many times per week will you do this habit?',
-            subtext: ''
-        })
-    }
-    const setHabitFrequency = (frequency) => {
-        frequency = parseInt(frequency)
-        updateHabit({ ...habit, frequency })
-        setShowFrequencySelection(false)
-        setShowEmojiSelection(true)
-        setSectionHeader({
-            title: 'Emoji',
-            subtext: 'Select an emoji to represent your habit. This will be represent your habit.'
-        })
-    }
-    const setHabitEmoji = (emoji) => {
-        updateHabit({ ...habit, emoji })
-        setShowEmojiSelection(false)
-        setShowHabitSummary(true)
-        setSectionHeader({
-            title: 'Habit Summary',
-            subtext: 'Review the following information and click save to create your new habit.'
-        })
-    }
-    const saveHabit = async () => {
-        const newHabit = await postHabit(userId, { ...habit, user: userId })
-        if (newHabit !== null) {
-            window.location.href = '/'
-        }
-    }
 
     return (
         <section className="flex flex-col rounded-md m-2 bg-colorBg text-colorText p-2">
