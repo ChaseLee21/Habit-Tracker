956a53af41d169527712b7b05ab8ea8e28ea8e22
Tiny Penguin <chase.seeberger@gmail.com>
2024-07-18 15:45:29 -0700

    update: emoji selection component now implements the new habit context api

--- client/src/components/screens/New Habit/components/EmojiSelection.jsx
+++ client/src/components/screens/New Habit/components/EmojiSelection.jsx
@@ -1,8 +1,10 @@
 import { React, useEffect, useState } from 'react'
-import PropTypes from 'prop-types'
 import 'emoji-picker-element'
+import SectionHeader from './SectionHeader'
+import { useNewHabit } from '../../../../contexts/NewHabitContext'
 
-function EmojiSelection ({ onItemClick }) {
+function EmojiSelection () {
+    const { updateHabit, updateShowEmojiSelection, updateShowHabitSummary } = useNewHabit()
     const [emoji, setEmoji] = useState('')
 
     useEffect(() => {
@@ -13,19 +15,27 @@ function EmojiSelection ({ onItemClick }) {
         })
     }, [])
 
+    function next () {
+        updateHabit({emoji: emoji})
+        updateShowHabitSummary(true)
+        updateShowEmojiSelection(false)
+    }
+
     return (
-        <div className='my-2'>
+        <div className='flex flex-col bg-colorBgAlt rounded w-fit p-2 my-2'>
+        <div className='mb-2'>
+            <SectionHeader title='Select an Emoji' subtext='This emoji will represent your habit through out different features of Habit Tracker!' />
+        </div>
+        <div className='mb-2'>
             <emoji-picker></emoji-picker>
-            <div className='flex flex-wrap bg-colorBgAlt rounded w-fit px-2 my-2'>
+        </div>
+            <div className='flex'>
                 <input className='rounded px-1 w-10 h-10 m-2 text-2xl text-black' id='emojiInput' type='text' value={emoji} readOnly></input>
-                <button className='bg-colorButtonBgAlt text-colorButtonTextAlt hover:text-colorLinkHover rounded w-fit p-1 my-2' onClick={() => onItemClick(emoji)}>Next</button>
+                {emoji && <button className='bg-colorButtonBgAlt text-colorButtonTextAlt hover:text-colorLinkHover rounded w-fit p-1 my-2' onClick={() => next()}>Next</button>}
+                {!emoji && <button className='bg-gray-600 text-gray-50 rounded w-fit p-1 my-2' disabled>Next</button>}
             </div>
         </div>
     )
-}
-
-EmojiSelection.propTypes = {
-    onItemClick: PropTypes.func.isRequired
 }
 
 export default EmojiSelection
