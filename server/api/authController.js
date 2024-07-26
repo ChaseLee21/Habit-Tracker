4651779ea97507573aebfdff22ae53fc6d2e36cd
Tiny Penguin <chase.seeberger@gmail.com>
2024-07-26 10:26:00 -0700

    feature: added post route at /reset-password that will send an email to the user with a link to reset their password at route /reset-password/{resetToken}

--- server/api/authController.js
+++ server/api/authController.js
@@ -1,6 +1,7 @@
 const { User } = require('../models')
 const { authMiddleware, signToken } = require('../utils/auth')
 const router = require('express').Router()
+const nodemailer = require('nodemailer')
 
 router.use('/login', (req, res) => {
     User.findOne({ email: req.body.email }).then(user => {
@@ -27,14 +28,44 @@ router.use('/checkToken', authMiddleware, async (req, res) => {
     res.json({ user: req.user })
 })
 
-router.use('/reset-password', async (req, res) => {
+router.post('/reset-password', async (req, res) => {
+    // Find user by email
     const user = await User.findOne({ email: req.body.email })
     if (!user) {
-        res.status(404).json({ message: 'No user with that email!' })
-        return 
+        return res.status(404).json({ message: 'No user with that email!' })
     }
-    // TODO: send email with reset link
-    res.status(200).json({ message: 'Email sent!' })
+
+    // Generate reset token
+    const resetToken = user.generateResetToken()
+    await user.save()
+
+    // Setup email transporter
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.EMAIL_PASSWORD
+        }
+    })
+
+    // Setup email data
+    const mailOptions = {
+        from: `"Habit Tracker" <${process.env.EMAIL}>`,
+        to: user.email,
+        subject: 'Reset your password',
+        text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n` +
+            `Please click on the following link, or paste this into your browser to complete the process:\n\n` +
+            `http://habit-tracker-jukt.onrender.com/reset-password/${resetToken}\n\n` +
+            `If you did not request this, please ignore this email and your password will remain unchanged.\n`
+    }
+
+    // Send email
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return res.status(500).json({ message: 'Error sending email' });
+        }
+        res.status(200).json({ message: 'Email sent!' });
+    });
 })
 
 module.exports = router
