4b01766bb36c88e2be96bafec1d4d66b2212dbae
Tiny Penguin <chase.seeberger@gmail.com>
2024-07-26 12:19:00 -0700

    feature: disable button after email is sent to prevent spamming the button

--- client/src/components/screens/Reset Password/ResetPassword.jsx
+++ client/src/components/screens/Reset Password/ResetPassword.jsx
@@ -13,6 +13,7 @@ function ResetPassword() {
 
     async function handleSendEmailSubmit (e) {
         e.preventDefault()
+        document.getElementById('sendEmailSubmitButton').disabled = true
         const email = { email: document.getElementById('email').value }
         const response = await sendResetPasswordEmail(email)
         console.log(response);
@@ -71,7 +72,7 @@ function ResetPassword() {
                 <form className="flex flex-col m-1 p-2 bg-colorBgAlt rounded shadow-md shadow-colorShadow text-lg" onSubmit={handleSendEmailSubmit}>
                     <label htmlFor="email" className="p-1 mx-1">Email:</label>
                     <input type="email" id="email" name="email" autoComplete='email' className="p-1 m-1 rounded-md" />
-                    <button type="submit" className="bg-colorButtonBgAlt text-colorButtonTextAlt w-fit p-2 rounded-md m-1">Send Email</button>
+                    <button type="submit" id="sendEmailSubmitButton" className="bg-colorButtonBgAlt text-colorButtonTextAlt w-fit p-2 rounded-md m-1">Send Email</button>
                 </form>
                 <p className="p-2 m-1"><Link to={'/login'} className='text-colorLink hover:text-colorLinkHover underline'>Back to Login</Link></p>
             </section>}
@@ -81,7 +82,7 @@ function ResetPassword() {
                     <input type="password" id="newPassword" name="newPassword" autoComplete='new-password' className="p-1 m-1 rounded-md" />
                     <label htmlFor="confirmPassword" className="p-1 mx-1">Confirm Password:</label>
                     <input type="password" id="confirmPassword" name="confirmPassword" className="p-1 m-1 rounded-md" />
-                    <button type="submit" className="bg-colorButtonBgAlt text-colorButtonTextAlt w-fit p-2 rounded-md m-1">Reset Password</button>
+                    <button type="submit" id="resetPasswordSubmitButton" className="bg-colorButtonBgAlt text-colorButtonTextAlt w-fit p-2 rounded-md m-1">Reset Password</button>
                 </form>
             </section>}  
 
