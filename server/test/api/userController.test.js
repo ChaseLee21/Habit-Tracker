c598e07c2396dbe3591ccbda3abe71b5b4478544
Tiny Penguin <chase.seeberger@gmail.com>
2024-08-07 14:17:40 -0700

    feat: added error handling to check for the server not initialized before running the tests

--- server/test/api/userController.test.js
+++ server/test/api/userController.test.js
@@ -1,15 +1,19 @@
 describe('User Controller', () => {
-    let chai, expect, request, app;
+    let chai, expect, request, server;
 
     before(async () => {
         chai = await import('chai');
         expect = chai.expect;
         request = (await import('supertest')).default;
-        app = (await import('../../server.js')).default;
+        const { serverReady } = require('../../server.js'); // Import the serverReady promise
+
+        // Wait for the server to be initialized
+        server = await serverReady;
+        if (!server) throw new Error('Server not initialized');
     });
 
     it('GET /api/user/:id should return a user with populated habits, weeks, and days', function (done) {
-        request(app)
+        request(server)
             .get('/api/user/6679dbb83f2d019488af9a21')
             .expect(200)
             .end(function (err, res) {
