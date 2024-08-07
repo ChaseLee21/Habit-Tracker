ebd1d1dd4eb7e91b064b06dac2b1ad95b2709b3d
Tiny Penguin <chase.seeberger@gmail.com>
2024-08-07 14:10:24 -0700

    feat: server.js now exports the server instance created from app.listen

--- server/server.js
+++ server/server.js
@@ -32,10 +32,15 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 // Connect to MongoDB
-db.once('open', () => {
-    console.log('Connected to MongoDB')
+db.on('error', console.error.bind(console, 'connection error:'));
 
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`)
-    })
-})
+let server;
+db.once('open', () => {
+    console.log('Connected to MongoDB');
+
+    server = app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+});
+
+module.exports = { app, server }; // Export both app and server
